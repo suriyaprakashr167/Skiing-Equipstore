190953068f91050728239470ff0eacf628f01a15
suriyaprakashr167 <suriyaprakashr167@gmail.com>
2025-10-14 17:40:11 +0530

    Redux RTK Q

--- api/Program.cs
+++ api/Program.cs
@@ -11,6 +11,16 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithOrigins("https://localhost:3000", "https://localhost:3001");
+    });
+});
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -20,6 +30,8 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,9 +7,15 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/routes/Routes.tsx';
+import { Provider } from 'react-redux';
+import { store } from './app/store/store';
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Provider store={store}>
     <RouterProvider router={router} />
-  </StrictMode>,
+    </Provider>
+  </StrictMode>
 )
